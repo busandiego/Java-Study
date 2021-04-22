3991140b7c8e700c2f07c704aeb8776f09feb9da
이수인 <silee@weedscomm.com>
2021-04-22 21:57:05 +0900

    레퍼런스타입, 프리미티브 타입 기본값 확인

--- src/main/java/com/lee/java_study/week2/PrimitiveType.java
+++ src/main/java/com/lee/java_study/week2/PrimitiveType.java
@@ -1,4 +1,33 @@
 package com.lee.java_study.week2;
 
 public class PrimitiveType {
+
+    int a ;
+
+    static class ReferenceType {
+
+    }
+
+    static ReferenceType referenceType2; // 이렇게하면 Null 나옴
+
+    public static void main(String[] args) {
+
+        PrimitiveType primitiveType = new PrimitiveType();
+
+        System.out.println(primitiveType.a);
+        ReferenceType referenceType = new ReferenceType(); // null이 아님
+        System.out.println(referenceType);
+        System.out.println(referenceType2);
+        if (primitiveType == null) {
+            System.out.println("null");
+        } else {
+            System.out.println("null 아님");
+        }
+
+        if (referenceType2 == null) {
+            System.out.println("null");
+        } else{
+            System.out.println("null 아님");
+        }
+    }
 }
